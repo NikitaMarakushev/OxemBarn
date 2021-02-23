6912feba0bc994f05a4fd1682101883c118eff4e
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-02-23 07:12:06 +0300

    beta version 0.0.1

--- main.php
+++ main.php
@@ -6,65 +6,89 @@ abstract  class AbstractAnimal
 
     abstract public function collectAnimalProduction();
 
-    abstract public function countCollectedProduction();
-
 }
 
-class Hens extends AbstractAnimal {
+class Hen extends AbstractAnimal {
 
     private $henRegistrationId;
 
-    private $producedEggs;
-
-    public function __construct($identification)
+    public function __construct()
     {
         $this->henRegistrationId = parent::$identification + 1;
     }
 
-    public function collectAnimalProduction()
+    public function collectAnimalProduction(): int
     {
-        // TODO: Implement collectAnimalProduction() method.
+        return $producedEggs = rand(0, 1);
     }
 
-    public function countCollectedProduction(): int
-    {
-        $producedEggs = rand(0, 1);
-        return $producedEggs;
-    }
 }
 
 class Cow extends AbstractAnimal {
 
     private $cowRegistrationId;
 
-    public $producedMilk;
 
-    public function __construct($identification)
+    public function __construct()
     {
         $this->cowRegistrationId = parent::$identification + 1;
     }
 
-    public function collectAnimalProduction()
+    public function collectAnimalProduction(): int
     {
-
-    }
-
-    public function countCollectedProduction(): int
-    {
-        $producedMilk = rand(8, 12);
-        return $producedMilk;
+        return $producedMilk = rand(8, 12);
     }
 
 }
 
-class Collector {
+class Barn {
 
     const MAX_COUNT_OF_COWS = 10;
     const MAX_COUNT_OF_HANS = 20;
 
-
-    public function addAnimalToBarn($animal)
+    public function addHenToBarn($hens)
     {
-        //Если в хлеву нет коровы ил курицы с таким номером - дабавить, есть - кинуть эксцепшн
+        if (count($hens) < self::MAX_COUNT_OF_HANS) {
+            return new Hen();
+        }
+    }
+
+    public function addCowToBarn($cows)
+    {
+        if (count($cows) < self::MAX_COUNT_OF_COWS) {
+            return new Cow();
+        }
+    }
+
+}
+
+$factory=new Barn();
+
+$barn = array();
+
+$hens = [];
+$cows = [];
+
+for($i=1;$i<=10;$i++){
+    $barn[]=$factory->addCowToBarn($cows);
+}
+
+for($i=1;$i<=20;$i++){
+    $barn[]=$factory->addHenToBarn($hens);
+}
+
+$milk=0;
+$egg=0;
+
+foreach ($barn as $value){
+    switch (get_class($value)) {
+        case "Cow":
+            $milk +=$value->collectAnimalProduction();
+            break;
+        case "Hen":
+            $egg +=$value->collectAnimalProduction();
+            break;
     }
 }
+echo "Milk-".$milk."\n";
+echo "Egg-".$egg."\n";
