5a52ce06417dbe97e8d80daf49459e5b6165d30c
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-02-23 05:43:44 +0300

    empty class produced

--- main.php
+++ main.php
@@ -1,2 +1,31 @@
 <?php
 
+class FoodProducer {
+
+    public function __construct()
+    {
+
+    }
+
+    public function registrationAnimal()
+    {
+
+    }
+
+    public function addToBarn()
+    {
+
+    }
+
+    public function collectProduction()
+    {
+
+    }
+
+    public function countProduction()
+    {
+
+    }
+
+
+}
